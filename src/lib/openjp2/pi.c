378ee3b3a818a996296019f43372fca416508a41
Even Rouault <even.rouault@spatialys.com>
2019-04-25 15:35:07 +0200

    opj_pi_update_encode_poc_and_final(): force layS = 0 (refs #1193)
    The standard mandates that the start layer of each POC must be 0.

--- src/lib/openjp2/pi.c
+++ src/lib/openjp2/pi.c
@@ -1078,28 +1078,7 @@ static void opj_pi_update_encode_poc_and_final(opj_cp_t *p_cp,
     /* start at first element, and to make sure the compiler will not make a calculation each time in the loop
        store a pointer to the current element to modify rather than l_tcp->pocs[i]*/
     l_current_poc = l_tcp->pocs;
-
-    l_current_poc->compS = l_current_poc->compno0;
-    l_current_poc->compE = l_current_poc->compno1;
-    l_current_poc->resS = l_current_poc->resno0;
-    l_current_poc->resE = l_current_poc->resno1;
-    l_current_poc->layE = l_current_poc->layno1;
-
-    /* special treatment for the first element*/
-    l_current_poc->layS = 0;
-    l_current_poc->prg  = l_current_poc->prg1;
-    l_current_poc->prcS = 0;
-
-    l_current_poc->prcE = p_max_prec;
-    l_current_poc->txS = (OPJ_UINT32)p_tx0;
-    l_current_poc->txE = (OPJ_UINT32)p_tx1;
-    l_current_poc->tyS = (OPJ_UINT32)p_ty0;
-    l_current_poc->tyE = (OPJ_UINT32)p_ty1;
-    l_current_poc->dx = p_dx_min;
-    l_current_poc->dy = p_dy_min;
-
-    ++ l_current_poc;
-    for (pino = 1; pino < l_poc_bound ; ++pino) {
+    for (pino = 0; pino < l_poc_bound ; ++pino) {
         l_current_poc->compS = l_current_poc->compno0;
         l_current_poc->compE = l_current_poc->compno1;
         l_current_poc->resS = l_current_poc->resno0;
@@ -1107,9 +1086,7 @@ static void opj_pi_update_encode_poc_and_final(opj_cp_t *p_cp,
         l_current_poc->layE = l_current_poc->layno1;
         l_current_poc->prg  = l_current_poc->prg1;
         l_current_poc->prcS = 0;
-        /* special treatment here different from the first element*/
-        l_current_poc->layS = (l_current_poc->layE > (l_current_poc - 1)->layE) ?
-                              l_current_poc->layE : 0;
+        l_current_poc->layS = 0;
 
         l_current_poc->prcE = p_max_prec;
         l_current_poc->txS = (OPJ_UINT32)p_tx0;
